c5da3c1ff54aa80eb12e5c70ab97a9a09c43da66
ryan-mcomber <ryanmcom@hotmail.com>
2021-09-15 15:15:02 -0600

    Connection Pooling done, added IdField from the example

--- diyORM/src/main/java/com/revature/util/IdField.java
+++ diyORM/src/main/java/com/revature/util/IdField.java
@@ -1,5 +1,31 @@
+
 package com.revature.util;
 
+import java.lang.reflect.Field;
+
+import com.revature.annotations.Id;
+
 public class IdField {
+
+    private Field field;
+
+    public IdField(Field field) {
+        if (field.getAnnotation(Id.class) == null) {
+            throw new IllegalStateException("Cannot create IdField object! Provided field, " + getName() + "is not annotated with @Id");
+        }
+        this.field = field;
+    }
+
+    public String getName() {
+        return field.getName();
+    }
+
+    public Class<?> getType() {
+        return field.getType();
+    }
+
+    public String getColumnName() {
+        return field.getAnnotation(Id.class).columnName();
+    }
 
 }
