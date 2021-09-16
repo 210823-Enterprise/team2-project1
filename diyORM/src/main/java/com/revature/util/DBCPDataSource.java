d6a4037d39068dd4a50574f9139ee49ae9cc2041
ryan-mcomber <ryanmcom@hotmail.com>
2021-09-16 09:32:32 -0600

    updated apache dbcp to newer version

--- diyORM/src/main/java/com/revature/util/DBCPDataSource.java
+++ diyORM/src/main/java/com/revature/util/DBCPDataSource.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.log4j.Logger;
 
 public class DBCPDataSource {
