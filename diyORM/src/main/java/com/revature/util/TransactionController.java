f1645de12a4b22f21de18bbeafcee60913d0aae9
Nathan-M-Lee <lee.nathan.m@gmail.com>
2021-09-24 17:19:57 -0700

    made some simple transactions

--- diyORM/src/main/java/com/revature/util/TransactionController.java
+++ diyORM/src/main/java/com/revature/util/TransactionController.java
@@ -1,36 +1,69 @@
 package com.revature.util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class TransactionController {
 
 	//begin databse commit.
-	public void beginCommit() {
+	public void beginCommit(Connection cn) {
 		
 	}
 	
 	//Enable auto commits on the database.
-	public void enableAutoCommit() {
+	public void enableAutoCommit(Connection cn) {
 		
 	}
 	
 	// Rollback to previous commit.
-	public void Rollback() {}
+	public void Rollback(Connection cn) {
+		String sql = "ROLLBACK";
+		try {
+			Statement stmt = cn.createStatement();
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	
 	// Rollback to previous commit with given name.
-	public void Rollback(final String name) {}
+	public void Rollback(final String name, Connection cn) {
+		String sql = "ROLLBACK TO " + name;
+		try {
+			Statement stmt = cn.createStatement();
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	
 	//Release the savepoint with the given name.
-	public void ReleaseSavepoint(final String name) {
-			
+	public void ReleaseSavepoint(final String name, Connection cn) {
+		String sql = "RELEASE SAVEPOINT " + name;
+		try {
+			Statement stmt = cn.createStatement();
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	//Set a savepoint with the given name.
-	public void setSavepoint(final String name) {
-		
+	public void setSavepoint(final String name, Connection cn) {
+		String sql = "SAVEPOINT " + name;
+		try {
+			Statement stmt = cn.createStatement();
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	//Start a transaction block.
-	public void setTransaction() {
+	public void setTransaction(Connection cn) {
 		
 	}
 }
